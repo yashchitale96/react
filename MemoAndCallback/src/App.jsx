3d9a096d73092a63024ebd7046030b5cfb0747d9
Yash Chitale <114438423+yashchitale96@users.noreply.github.com>
2026-08-30 02:50:46 +0530

    Update App.jsx

--- MemoAndCallback/src/App.jsx
+++ MemoAndCallback/src/App.jsx
@@ -3,22 +3,36 @@ import {useState} from 'react'
 import Sum from './components/Sum.jsx'
 const App = () => {
     const [counter, setCounter] = useState(0);
+    const [number, setNumber] = useState(0);
     console.log('App re-render')
 
-    function bigLoop(){
-      for(let i=0; i<1e7; i++){
+    const prime = useMemo(()=>{
+    
+    let total = 0;
 
+    if(number>1)
+      total++;
+
+    for(let i=3;i<=number;i++){
+      total++;
+      for(let j=2;j<i;j++){
+        if(i%j==0){
+          total--;
+          break;
+        }
       }
     }
 
-    const var1 = bigLoop();
+    return total;
+
+  },[number])
   return (
     <div>
         <h1>Counter</h1>
         <p>{counter}</p>
         <button onClick={()=>setCounter(counter+1)}>Increment</button>
         <Sum number={counter}/>
-        <h1>{var1}</h1>
+        <h1>{prime}</h1>
     </div>
   )
 }
